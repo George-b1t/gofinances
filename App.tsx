c3f7e3c72f0169586f94bfb421c223e08c035f0d
George-b1t <george.soares509@gmail.com>
2022-09-07 15:32:27 -0300

    Criando componentes com styled-components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Dashboard />
     </View>
   );
 }
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "./styles";
+
+function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
+
+export { Dashboard };
