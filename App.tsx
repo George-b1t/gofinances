4b32cffc6e6e699c09b3d69ed201e0fa5fb58863
George-b1t <george.soares509@gmail.com>
2022-09-28 10:10:29 -0300

    Utilizando o Tab Navigation

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import {
 
 import theme from "./src/global/styles/theme";
 
-import { Register } from "./src/screens/Register";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +24,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
